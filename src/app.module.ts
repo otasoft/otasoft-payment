a1da2749d447d809645d413712954ea7d10ae0a7
Adam Oleszko <adamoleszko44@gmail.com>
2021-01-24 12:03:14 +0100

    feat-#15: Update app.module and main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+import { DbModule } from './db/db.module';
+import { PaymentModule } from './payment/payment.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), DbModule, PaymentModule, HealthModule],
 })
 export class AppModule {}
