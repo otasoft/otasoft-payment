55314fc97e57164b6b166fdb753ff4f7fdd901e3
Adam Oleszko <adamoleszko44@gmail.com>
2021-01-24 11:58:27 +0100

    feat-#15: Add create payment handler

---
+++ src/payment/commands/handlers/create-payment.handler.ts
@@ -0,0 +1,33 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RpcException } from '@nestjs/microservices';
+
+import { CreatePaymentCommand } from '../impl';
+import { PaymentRepository } from '../../repositories/payment.repository';
+
+@CommandHandler(CreatePaymentCommand)
+export class CreatePaymentHandler
+  implements ICommandHandler<CreatePaymentCommand> {
+  constructor(
+    @InjectRepository(PaymentRepository)
+    private readonly paymentRepository: PaymentRepository,
+  ) {}
+
+  async execute(command: CreatePaymentCommand) {
+    const { customer_id, booking_id } = command.createPaymentDto;
+    // TODO: add booking validation
+
+    const payment = await this.paymentRepository.create();
+
+    payment.booking_id = booking_id;
+    payment.customer_id = customer_id;
+    payment.created_at = new Date();
+
+    try {
+      await payment.save();
+      return payment;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+}
