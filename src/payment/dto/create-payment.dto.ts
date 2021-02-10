7cf4db57e578696a828079699ae16d96a02e8c0e
Adam Oleszko <adamoleszko44@gmail.com>
2021-02-11 00:20:00 +0100

    feat-#4: Update createPaymentDto

--- src/payment/dto/create-payment.dto.ts
+++ src/payment/dto/create-payment.dto.ts
@@ -1,8 +1,17 @@
-import { IsNumber } from 'class-validator';
+import { IsLowercase, IsNumber, IsString, Length } from 'class-validator';
 
 class Payment {
   @IsNumber()
   booking_id: number;
+
+  @IsNumber()
+  amount: number;
+
+  // TODO: maybe we should have a list of valid currencies in the db
+  @IsString()
+  @Length(3, 3)
+  @IsLowercase()
+  currency: string;
 }
 
 class Booking {}
@@ -10,4 +19,7 @@ class Booking {}
 export class CreatePaymentDto {
   newPayment: Payment;
   booking: Booking;
+
+  @IsString()
+  cardToken: string;
 }
